f0c0b85d80b2bcb922077231459f39db78ac88be
Alejandro Amaya <a.amayaa2@uniandes.edu.co>
2025-09-10 19:29:46 -0500

    JSON

--- src/uniandes/dpoo/aerolinea/persistencia/IPersistenciaAerolinea.java
+++ src/uniandes/dpoo/aerolinea/persistencia/IPersistenciaAerolinea.java
@@ -1,5 +1,8 @@
 package uniandes.dpoo.aerolinea.persistencia;
 
+import org.json.JSONException;
+
+import uniandes.dpoo.aerolinea.exceptions.AeropuertoDuplicadoException;
 import uniandes.dpoo.aerolinea.modelo.Aerolinea;
 
 public interface IPersistenciaAerolinea {
